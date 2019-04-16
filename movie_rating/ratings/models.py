d304fd34604bf9db853f2fe1eca9fadea845891b
cberry216 <christopherberry216@gmail.com>
2019-04-16 10:30:51 -0400

    Added group models.

--- movie_rating/ratings/models.py
+++ movie_rating/ratings/models.py
@@ -10,3 +10,50 @@ class User(AbstractUser):
     first_name = models.CharField(max_length=60)
     last_name = models.CharField(max_length=80, null=True, blank=True)
     email = models.CharField(max_length=120)
+
+
+class Group(models.Model):
+    group_id = models.IntegerField(primary_key=True)
+    group_name = models.CharField(max_length=60, unique=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+class GroupAdmin(models.Model):
+    group_admin_id = models.IntegerField(primary_key=True)
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.CASCADE,
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
+
+
+class Invitation(models.Model):
+    INVITATION_OPTIONS = [
+        ('sent', 'sent'),
+        ('ignored', 'ignored')
+    ]
+    invitation_id = models.IntegerField(primary_key=True)
+    sender = models.ForeignKey(
+        User,
+        related_name='sent_invitations',
+        on_delete=models.CASCADE
+    )
+    receiver = models.ForeignKey(
+        User,
+        related_name='received_invitations',
+        on_delete=models.CASCADE
+    )
+    group = models.ForeignKey(
+        Group,
+        related_name='invitations_to_join',
+        on_delete=models.CASCADE
+    )
+    status = models.CharField(
+        max_length=8,
+        choices=INVITATION_OPTIONS,
+        default='sent'
+    )
+    sent = models.DateTimeField(auto_now_add=True)
