c4e3c91ace263bc5476124e7f7a53bd5aea774b2
cberry216 <christopherberry216@gmail.com>
2019-04-16 10:41:02 -0400

    Added models to admin.

--- movie_rating/ratings/admin.py
+++ movie_rating/ratings/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import (
+    User,
+    Group,
+    GroupAdminMember,
+    Invitation
+)
 
 # Register your models here.
 admin.site.register(User, UserAdmin)
+
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ['group_id', 'group_name', 'created']
+
+
+@admin.register(GroupAdminMember)
+class GroupAdminMemberAdmin(admin.ModelAdmin):
+    list_display = ['group_admin_id', 'group', 'user']
+
+
+@admin.register(Invitation)
+class InvitationAdmin(admin.ModelAdmin):
+    list_display = ['invitation_id', 'sender', 'receiver', 'group', 'status']
