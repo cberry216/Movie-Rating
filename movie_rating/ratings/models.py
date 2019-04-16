160d31c5975aa5733a9beea679118192fa144838
cberry216 <christopherberry216@gmail.com>
2019-04-16 10:56:45 -0400

    Added Movie to ratings models.

--- movie_rating/ratings/models.py
+++ movie_rating/ratings/models.py
@@ -63,3 +63,31 @@ class Invitation(models.Model):
         default='sent'
     )
     sent = models.DateTimeField(auto_now_add=True)
+
+
+class Movie(models.Model):
+    MOVIE_RATINGS = [
+        ('G', 'G'),
+        ('PG', 'PG'),
+        ('PG-13', 'PG-13'),
+        ('R', 'R'),
+        ('NC-17', 'NC-17'),
+    ]
+    movie_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    rated = models.CharField(
+        max_length=5,
+        choices=MOVIE_RATINGS,
+        default='PG-13',
+        null=True,
+        blank=True
+    )
+    released = models.DateField()
+    runtime_minutes = models.IntegerField()
+    genre = models.CharField(max_length=20, null=True, blank=True)
+    director = models.CharField(max_length=80, null=True, blank=True)
+    plot = models.TextField(null=True, blank=True)
+    poster_link = models.CharField(max_length=250, null=True, blank=True)
+    imdb_rating = models.FloatField(null=True, blank=True)
+    rt_rating = models.IntegerField(null=True, blank=True)
+    added_to_db = models.DateTimeField(auto_now_add=True)
