3b50a78ab0ba185faafc80c46fdbf7b097c5e331
cberry216 <christopherberry216@gmail.com>
2019-04-16 09:11:14 -0400

    Removed testing from installed apps.

--- movie_rating/movie_rating/settings.py
+++ movie_rating/movie_rating/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'testing.apps.TestingConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
