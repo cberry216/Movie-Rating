eeba376e58e18bc53241536a8924b8a02683b251
cberry216 <christopherberry216@gmail.com>
2019-04-16 09:34:57 -0400

    Added custom user model.

--- movie_rating/user/models.py
+++ movie_rating/user/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    user_id = models.IntegerField(primary_key=True)
+    username = models.CharField(max_length=60, unique=True)
+    first_name = models.CharField(max_length=60)
+    last_name = models.CharField(max_length=80, null=True, blank=True)
+    email = models.CharField(max_length=120)
