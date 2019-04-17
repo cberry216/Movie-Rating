2e3f06d965860b5256008e0c36122739e81333d6
cberry216 <christopherberry216@gmail.com>
2019-04-17 10:17:20 -0400

    Made function for getting unrated group movies.

--- movie_rating/ratings/helpers.py
+++ movie_rating/ratings/helpers.py
@@ -1,8 +1,21 @@
 from django.core.exceptions import ObjectDoesNotExist
 
+from .models import Group, Rating, Movie
+
 
 def get_item_or_none(model, **kwargs):
     try:
         return model.objects.get(**kwargs)
     except ObjectDoesNotExist:
+        return None
+
+
+def get_unrated_movies_from_group(user):
+    if user.group:
+        group = get_item_or_none(Group, group_id=user.group.group_id)
+        group_ratings = Rating.objects.filter(user__in=group.users.all())
+        group_movies = Movie.objects.filter(movie_id__in=group_ratings.values_list('movie__movie_id'))
+        user_movies = Movie.objects.filter(movie_id__in=user.ratings.values_list('movie__movie_id'))
+        return [movie for movie in group_movies if movie not in user_movies]
+    else:
         return None
