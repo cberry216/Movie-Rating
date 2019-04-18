511a49f1da7c2e506136fc94e8f330482352cd6a
cberry216 <christopherberry216@gmail.com>
2019-04-18 09:19:52 -0400

    Added registration, password change URLConfs.

--- movie_rating/ratings/user_urls.py
+++ movie_rating/ratings/user_urls.py
@@ -1,7 +1,10 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from .views import register
 
 urlpatterns = [
-    path('register/', register, name='register')
+    path('register/', register, name='register'),
+    path('password_change/', auth_views.PasswordChangeView.as_view, name='password_change'),
+    path('password_change/done', auth_views.PasswordChangeDoneView.as_view, name='password_change_done'),
 ]
