3774fb1c9ecd278e2a67328088e4c950fb2a1615
cberry216 <christopherberry216@gmail.com>
2019-04-18 11:46:51 -0400

    Added redirect for logged in users

--- movie_rating/ratings/urls.py
+++ movie_rating/ratings/urls.py
@@ -5,7 +5,7 @@ from .views import homepage
 
 urlpatterns = [
     path('', homepage, name='homepage'),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('login/', auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('movie/', include('ratings.movie_urls')),
     path('user/', include('ratings.user_urls')),
