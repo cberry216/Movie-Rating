6fc942cbbbee00da2e2c16efbd8b0a929fcd76ba
cberry216 <christopherberry216@gmail.com>
2019-04-18 09:33:25 -0400

    Fixed urls not being included.

--- movie_rating/ratings/urls.py
+++ movie_rating/ratings/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', homepage, name='homepage'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('movie/', include('movie_urls')),
-    path('user/', include('user_urls')),
+    path('movie/', include('ratings.movie_urls')),
+    path('user/', include('ratings.user_urls')),
 ]
