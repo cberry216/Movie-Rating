4afd9e8bec7459d01be81b05853e5767a7bac5e0
cberry216 <christopherberry216@gmail.com>
2019-04-18 08:57:29 -0400

    Added user registration form.

--- movie_rating/ratings/forms.py
+++ movie_rating/ratings/forms.py
@@ -1,5 +1,22 @@
 from django import forms
 
+from .models import User
+
 
 class SearchForm(forms.Form):
     query = forms.CharField()
+
+
+class UserRegistrationForm(form.ModelForm):
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat Password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email')
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Passwords don\'t match.')
+        return cd['password2']
