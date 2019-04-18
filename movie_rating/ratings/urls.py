f683e9a1eecee1ab612a63f78d0b4c7e1dd9f8fb
cberry216 <christopherberry216@gmail.com>
2019-04-18 09:19:40 -0400

    Added login and logout URLConfs.

--- movie_rating/ratings/urls.py
+++ movie_rating/ratings/urls.py
@@ -1,8 +1,12 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 
 from .views import homepage
 
 urlpatterns = [
     path('', homepage, name='homepage'),
-    path('movie/', include('movie-urls'))
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('movie/', include('movie_urls')),
+    path('user/', include('user_urls')),
 ]
