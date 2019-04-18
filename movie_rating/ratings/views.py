13c4c6b2b86e08a560075ee40369bfc1820a5c73
cberry216 <christopherberry216@gmail.com>
2019-04-18 08:57:51 -0400

    Created registration view.

--- movie_rating/ratings/views.py
+++ movie_rating/ratings/views.py
@@ -3,7 +3,7 @@ import requests
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-from .forms import SearchForm
+from .forms import SearchForm, UserRegistrationForm
 from .helpers import get_unrated_movies_from_group
 
 # Create your views here.
@@ -11,6 +11,19 @@ from .helpers import get_unrated_movies_from_group
 
 def homepage(request):
     return render(request, 'home.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            return render(request, 'registration/register_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+        return render(request, 'registration/register.html', {'user_form': user_form})
 
 
 @login_required
