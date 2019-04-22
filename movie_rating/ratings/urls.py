fd094e82edf411c4acb7dfb46bd95269bff72a8e
cberry216 <christopherberry216@gmail.com>
2019-04-22 12:15:07 -0400

    Created URLConf for dashboard.

--- movie_rating/ratings/urls.py
+++ movie_rating/ratings/urls.py
@@ -1,10 +1,11 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 
-from .views import homepage
+from .views import homepage, dashboard
 
 urlpatterns = [
     path('', homepage, name='homepage'),
+    path('dashboard/', dashboard, name='dashboard'),
     path('login/', auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('movie/', include('ratings.movie_urls')),
