9c524bbd6f61fa5729ac0642044adcafd92d9725
cberry216 <christopherberry216@gmail.com>
2019-05-02 13:19:35 -0400

    Added URLConf for movie rating.

--- movie_rating/ratings/movie_urls.py
+++ movie_rating/ratings/movie_urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from .views import (
     search_movie,
-    movie_detail
+    movie_detail,
+    rate_movie,
 )
 
 urlpatterns = [
-    path('rate-movie/', search_movie, name='rate-movie'),
+    path('rate-movie/', search_movie, name='search_movie'),
+    path('rate-movie/<imdb_id>', rate_movie, name='rate_movie'),
     path('<imdb_id>/', movie_detail, name='movie_detail'),
 ]
