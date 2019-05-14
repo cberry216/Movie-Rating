bbd09f6c85c2b79d6b85c5e7315c4048fd5ee21c
cberry216 <christopherberry216@gmail.com>
2019-05-14 13:38:35 -0400

    Created function to parse omdb response.

--- movie_rating/ratings/helpers.py
+++ movie_rating/ratings/helpers.py
@@ -1,5 +1,7 @@
 from django.core.exceptions import ObjectDoesNotExist
 
+from datetime import datetime
+
 from .models import Group, Rating, Movie
 
 
@@ -33,6 +35,79 @@ def user_has_rated_movie(user, movie):
     return user_rating_for_movie is not None
 
 
+def user_has_rated_movie_by_id(user, imdb_id):
+    movie = get_item_or_none(Movie, imdb_id=imdb_id)
+    if movie:
+        return user_has_rated_movie(user, movie)
+
+
 def movie_in_database(imdb_id):
     movie = get_item_or_none(Movie, imdb_id=imdb_id)
     return movie is not None
+
+
+def parse_movie_json(omdb_resp):
+    # CHAR - NOT NULL - ex. "tt5884052"
+    imdb_id = omdb_resp['imdbID']
+
+    # CHAR - NOT NULL - ex. "The Curious Case of Benjamin Button"
+    title = omdb_resp['Title']
+
+    # CHAR - NOT NULL - ex. "PG"
+    rated = omdb_resp['Rated']
+
+    # DATE - NOT NULL - ex. "09 May 2019"
+    released = datetime.strptime(omdb_resp['Released'], "%d %b %Y")
+
+    # INTEGER - NOT NULL - ex. "104 min"
+    runtime_minutes = int(omdb_resp['Runtime'].split(' ')[0])
+
+    # CHAR - NULL - ex. "Action, Adventure"
+    if omdb_resp['Genre'] != 'N/A':
+        genre = omdb_resp['Genre'].split(',')[0]
+    else:
+        genre = None
+
+    # CHAR - NULL - ex: "David Fincher"
+    if omdb_resp['Director'] != 'N/A':
+        director = omdb_resp['Director']
+    else:
+        director = None
+
+    # TEXT - NULL - ex: "Journalist Mikael Blomkvist is aided in his search..."
+    if omdb_resp['Plot'] != 'N/A':
+        plot = omdb_resp['Plot']
+    else:
+        plot = None
+
+    # CHAR - NULL - ex: "https://m.media-amazon.com/images/M..."
+    if omdb_resp['Poster'] != 'N/A':
+        poster_link = omdb_resp['Poster']
+    else:
+        poster_link = None
+
+    # DECIMAL - NULL - ex: "9.3"
+    if omdb_resp['imdbRating']:
+        imdb_rating = float(omdb_resp['imdbRating'])
+    else:
+        imdb_rating = None
+
+    # INTEGER - NULL - ex: "86%"
+    if len(omdb_resp['Ratings']) >= 2:
+        rt_rating = int(omdb_resp['Ratings'][1]['Value'].split('%')[0])
+    else:
+        rt_rating = None
+
+    return Movie(
+        imdb_id=imdb_id,
+        title=title,
+        rated=rated,
+        released=released,
+        runtime_minutes=runtime_minutes,
+        genre=genre,
+        director=director,
+        plot=plot,
+        poster_link=poster_link,
+        imdb_rating=imdb_rating,
+        rt_rating=rt_rating
+    )
