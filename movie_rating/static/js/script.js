9c2de5a7ebba36fbd916058e570e3667f166e868
cberry216 <christopherberry216@gmail.com>
2019-05-19 19:13:21 -0400

    Added color changing to user rating and diffs.

--- movie_rating/static/js/script.js
+++ movie_rating/static/js/script.js
@@ -1,5 +1,30 @@
+function setRatingColor() {
+  console.log('Setting rating color...');
+  let userRatingElement = document.querySelector('.rating__user-rating');
+  let userRating = Number.parseFloat(userRatingElement.innerHTML);
+  greenFactor = (25.5 * userRating);
+  redFactor = 255 - greenFactor;
+  userRatingElement.setAttribute('style', `color: rgb(${redFactor},${greenFactor},0)`);
+}
+
+function setDeltaColor() {
+  console.log('Setting delta colors...');
+  let ratingDiffs = document.querySelectorAll('.rating__diff');
+  for(let i = 0; i < ratingDiffs.length; i++) {
+    let text = ratingDiffs[i].innerHTML;
+    let value = Number.parseFloat(text);
+    if(value >= 0) {
+      ratingDiffs[i].classList.toggle('rating__diff--positive');
+    }
+    else {
+      ratingDiffs[i].classList.toggle('rating__diff--negative');
+    }
+  }
+}
+
 function run() {
-  // removeDarknessAfterFadeOut();
+  setRatingColor();
+  setDeltaColor();
 }
 
 window.onload = run;
