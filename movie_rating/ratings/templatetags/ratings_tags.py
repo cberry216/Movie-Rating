272bf717170970c927c3ff44980a02789c5ae775
cberry216 <christopherberry216@gmail.com>
2019-05-19 15:18:10 -0400

    Added minus rating tag.

--- movie_rating/ratings/templatetags/ratings_tags.py
+++ movie_rating/ratings/templatetags/ratings_tags.py
@@ -6,3 +6,8 @@ register = template.Library()
 @register.simple_tag
 def strip_page(querystring):
     return querystring.split('&page')[0]
+
+
+@register.simple_tag
+def minus(a, b):
+    return int(a) - int(b)
