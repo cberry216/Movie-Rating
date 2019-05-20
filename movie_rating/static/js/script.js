64c99e323d39332c1352988a93ed580873cf29f1
cberry216 <christopherberry216@gmail.com>
2019-05-19 20:09:57 -0400

    Fixed renaming classes.

--- movie_rating/static/js/script.js
+++ movie_rating/static/js/script.js
@@ -1,28 +1,40 @@
 function setRatingColor() {
   console.log('Setting rating color...');
-  let userRatingElement = document.querySelector('.rating__user-rating');
+  let userRatingElement = document.querySelector('.user-rating__user-rating');
+  let memberRatingElements = document.querySelectorAll('.member-rating__value');
+
   let userRating = Number.parseFloat(userRatingElement.innerHTML);
-  greenFactor = (25.5 * userRating);
-  redFactor = 255 - greenFactor;
+  let greenFactor = 25.5 * userRating;
+  let redFactor = 255 - greenFactor;
   userRatingElement.setAttribute('style', `color: rgb(${redFactor},${greenFactor},0)`);
+
+  for(let i = 0; i < memberRatingElements.length; i++) {
+    let memberRating = Number.parseFloat(memberRatingElements[i].innerHTML);
+    let greenFactor = 25.5 * memberRating;
+    let redFactor = 255 - greenFactor;
+    memberRatingElements[i].setAttribute('style', `color: rgb(${redFactor},${greenFactor},0)`);
+  }
 }
 
 function setDeltaColor() {
   console.log('Setting delta colors...');
-  let ratingDiffs = document.querySelectorAll('.rating__diff');
+  let ratingDiffs = document.querySelectorAll('.user-rating__diff');
   let memberRatingDiffs = document.querySelectorAll('.member-rating__diff');
   for(let i = 0; i < ratingDiffs.length; i++) {
     let text = ratingDiffs[i].innerHTML;
     let value = Number.parseFloat(text);
     if(value >= 0) {
-      ratingDiffs[i].classList.toggle('rating__diff--positive');
+      ratingDiffs[i].classList.toggle('user-rating__diff--positive');
     }
     else {
-      ratingDiffs[i].classList.toggle('rating__diff--negative');
+      ratingDiffs[i].classList.toggle('user-rating__diff--negative');
     }
   }
   for (let i = 0; i < memberRatingDiffs.length; i++) {
     let text = memberRatingDiffs[i].innerHTML;
+    if(text.includes("N/A")) {
+      continue;
+    }
     let value = Number.parseFloat(text);
     if (value >= 0) {
       memberRatingDiffs[i].classList.toggle('rating__diff--positive');
